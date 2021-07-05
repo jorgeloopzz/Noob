93373e598ef74090a8b736d0c1ae7fd966a5a36f
Jorge López <80071604+Jorgedeveloopzz@users.noreply.github.com>
2021-07-05 17:44:01 +0100

    Update dropButton.js

--- js/dropButton.js
+++ js/dropButton.js
@@ -1,17 +1,25 @@
-// To drop the stats
-function myFunction() {
-    document.getElementById('gamesDrop').classList.toggle('show')
-}
+// // To drop the stats
 
-window.onclick = function(event) {
-    if (!event.target.matches('.stats')) {
-        var dropdowns = document.getElementsByClassName('gamesDropdown');
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show')
-            }
-        }
-    }
-}
+document-addEventListener('DOMContentLoaded', () => {
+
+    // Show drop-menu
+    let dropBtn = document.getElementById('gamesButton');
+
+    dropBtn-addEventListener('click', () => {
+        document.getElementById('gamesDrop').classList.toggle('show');
+    })
+
+    // Hide drop-menu
+    window.onclick = function(event) {
+        if (!event.target.matches('.stats')) {
+            var dropdowns = document.getElementsByClassName('gamesDropdown');
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                   openDropdown.classList.remove('show')
+               }
+           }
+       }
+   }
+});
