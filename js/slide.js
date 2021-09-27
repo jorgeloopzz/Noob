017f6e61dd1c973d102164012748c3c7ef5fb3b6
Jorgedeveloopzz <jorgelpv88@gmail.com>
2021-09-27 21:04:29 +0100

    Functionality to the buttons of the header to show content

--- js/slide.js
+++ js/slide.js
@@ -1,4 +1,13 @@
 // To choose the content you wanna see (About me, Stats...)
-document.addEventListener('DOMCOntentLoaded', () => {
+
+function slideShows(n) {
+    let slides = document.getElementsByClassName('content');
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
     
-});
+    slides[n -1].style.display = "block";
+
+    return slides
+};
