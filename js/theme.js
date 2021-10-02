0e4e8e978a33e05fc322bb2edf9a47bc81bcd65d
Jorgedeveloopzz <jorgelpv88@gmail.com>
2021-10-02 21:56:45 +0100

    Change the foor loop

--- js/theme.js
+++ js/theme.js
@@ -9,9 +9,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let bg = document.body;
     let h1 = document.querySelector("h1");
     let icons = document.querySelectorAll("i");
-    let header = document.getElementById("insideHeader");
     let buttons = document.querySelectorAll(".header-button") 
-    var borderBtn = document.querySelectorAll(".border");
+    let borderBtn = document.querySelectorAll(".border");
+    let header = document.getElementById("insideHeader");
     const thmBtn = document.getElementById("changeThemeButton");
 
     thmBtn.addEventListener("click", () => {
@@ -19,40 +19,38 @@ document.addEventListener('DOMContentLoaded', () => {
         // Change to light color 
 
         if (bg.style.backgroundColor === "" || bg.style.backgroundColor === darkColor) {
-            bg.style.backgroundColor = lightColor;
+            bg.style.backgroundColor = lightColor; // Page color
             h1.style.color = darkColor; // Color of the title
             header.style.backgroundColor = lightColor; // Background header
-            // Icons color
+
+            // Icons, buttons and border buttons light color
             for (let i = 0; i < icons.length; i++) {
+                for (let j = 0; j < buttons.length; j++) {
+                    for (let z = 0; z < borderBtn.length; z++) {
+                        borderBtn[z].style.backgroundColor = darkColor;                
+                    }
+                    buttons[j].style.color = darkColor;
+                }
                 icons[i].style.color = darkColor;
-            }
-            // Buttons color
-            for (let j = 0; j < buttons.length; j++) {
-                buttons[j].style.color = darkColor;
-            }
-            // Border btns color
-            for (let z = 0; z < borderBtn.length; z++) {
-                borderBtn[z].style.backgroundColor = darkColor;
             }
         }
         
         // Change to dark color
         
         else {
-            bg.style.backgroundColor = darkColor;
+            bg.style.backgroundColor = darkColor; // Page color
             h1.style.color = lightColor; // Color of the title
             header.style.backgroundColor = darkColor; // Background header
-            // Icons color
+
+            // Icons, buttons and border buttons dark color
             for (let i = 0; i < icons.length; i++) {
+                for (let j = 0; j < buttons.length; j++) {
+                    for (let z = 0; z < borderBtn.length; z++) {
+                        borderBtn[z].style.backgroundColor = lightColor;
+                    }
+                    buttons[j].style.color = lightColor;
+                }
                 icons[i].style.color = lightColor;
-            }
-            // Buttons color
-            for (let j = 0; j < buttons.length; j++) {
-                buttons[j].style.color = lightColor;
-            }
-            // Border btns color
-            for (let z = 0; z < borderBtn.length; z++) {
-                borderBtn[z].style.backgroundColor = lightColor;
             }
         }
     })
